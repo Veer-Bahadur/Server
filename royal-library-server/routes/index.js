2367c075aa09a69acc3c85da4a571dbf0095c2bd
AmanTiwari <er.amantiwari.2002@gmail.com>
2024-07-21 18:32:35 +0530

    Seat Allocation Completed

--- royal-library-server/routes/index.js
+++ royal-library-server/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addStudent, getAllStudents, getEnrollmentNo } from '../controller/HomeController.js'
+import { addStudent, getAllStudents, getEnrollmentNo, getSeatInfo } from '../controller/HomeController.js'
 const router = express.Router()
 
 router.post("/addStudent",addStudent)
 router.get("/getEnrollmentNo",getEnrollmentNo)
 router.get("/getAllStudents",getAllStudents)
+router.get("/getSeatInfo",getSeatInfo)
 
 export default router
